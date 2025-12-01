6593224bfd643198120a10c0b312cb8da4ea4478
Noah Manaigre <43097168+NoahPMan@users.noreply.github.com>
2025-12-01 15:08:37 -0600

    Implement DynamoDB query in lambda function

--- lambda/get_location_inventory_items/lambda_function.py
+++ lambda/get_location_inventory_items/lambda_function.py
@@ -1,1 +1,30 @@
+import json
+import boto3
+import os
 
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ.get('TABLE_NAME', 'Inventory')
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event, context):
+    try:
+        location_id = int(event['pathParameters']['id'])
+        
+        response = table.query(
+            IndexName='LocationIndex',  # Replace with your GSI name
+            KeyConditionExpression='location_id = :loc',
+            ExpressionAttributeValues={':loc': location_id}
+        )
+        
+        items = response.get('Items', [])
+        
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps(items)
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }

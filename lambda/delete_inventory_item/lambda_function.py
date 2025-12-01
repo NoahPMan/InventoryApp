5169f39eea0bb3370977448551ec12d0a39a20ac
Noah Manaigre <43097168+NoahPMan@users.noreply.github.com>
2025-12-01 15:08:17 -0600

    Add delete_item functionality to lambda_handler
    Implement delete functionality for inventory items in DynamoDB.

--- lambda/delete_inventory_item/lambda_function.py
+++ lambda/delete_inventory_item/lambda_function.py
@@ -1,1 +1,23 @@
+import json
+import boto3
+import os
 
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ.get('TABLE_NAME', 'Inventory')
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event, context):
+    try:
+        item_id = event['pathParameters']['id']
+        
+        table.delete_item(Key={'id': item_id})
+        
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'message': 'Item deleted'})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
